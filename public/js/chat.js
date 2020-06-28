8c26e2c2413b2404e5d2e66755b79a462af331a5
Derek <derekluu90@gmail.com>
2020-06-28 15:38:53 -0400

    added auto scrolling when user is at the bottom of the page

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,42 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // QS parses query string. Query string is created when a user submits a display name / room (key names determined with name attribute in html)
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoscroll = () => {
+  // new messages are added to the bottom of the div
+  const $newMessage = $messages.lastElementChild
+
+  // Height of the new message
+  // getComputedStyle is a method of window object - returns an object with css styles
+  const newMessageStyles = getComputedStyle($newMessage)
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  // offsetHeight returns height of element including vertical padding and border (excluding margin)
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+  // Visible height of container
+  const visibleHeight = $messages.offsetHeight
+
+  // Height of messages container
+  // total available height to scroll through
+  const containerHeight = $messages.scrollHeight
+
+  // How far have I scrolled?
+  // how far scrolled from the top - the distance from the top of the SCROLLBAR to the top of the CONTENT
+  // height of the visible container = scrollbar height
+  // the sum is the height from the bottom of the scroll bar to the top of the content
+  const scrollOffset = $messages.scrollTop + visibleHeight
+
+  // determine if scrolled to the bottom BEFORE new message added - if we dont account for the new message we will never be scrolled to the bottom
+  // because we would be running this code AFTER the newMessageHeight is added to containerHeight (containerHeight includes all of the newMessageHeights)
+  // total height of the container - newMessageHeight
+
+  // if the height of the container (beforeNewMessages)  is less than the height scrolled from the top of the content 
+  // then autoscroll. checking if user is at bottom of the container before the last message
+  if (containerHeight - newMessageHeight <= scrollOffset){
+    // scrollTop (top of scrollbar to top of content) set to equal to the total available scroll height (all the way to the bottom)
+    $messages.scrollTop = $messages.scrollHeight
+  }
+}
+
 socket.on('message', ({ username, text, createdAt }) => {
   // render with template. second argument is object where the keys can be referenced in the script template
   const html = Mustache.render(messageTemplate, {
@@ -26,6 +62,7 @@ socket.on('message', ({ username, text, createdAt }) => {
     createdAt: moment(createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoscroll()
 })
 
 socket.on('locationMessage', ({ username, url, createdAt }) => {
